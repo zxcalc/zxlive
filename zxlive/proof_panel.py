6f8e256db7d8c8d2e8a0dfd398e11714b762861b
Mark Koch <mark.koch@quantinuum.com>
2023-07-27 21:30:32 +0100

    Fix mouse hover

--- zxlive/proof_panel.py
+++ zxlive/proof_panel.py
@@ -49,6 +49,7 @@ class ProofPanel(BasePanel):
         self.step_view.setItemDelegate(ProofStepItemDelegate())
         self.step_view.setCurrentIndex(self.proof_model.index(0, 0))
         self.step_view.selectionModel().selectionChanged.connect(self._proof_step_selected)
+        self.step_view.viewport().setAttribute(Qt.WA_Hover)
 
         self.panel_widget.layout().addWidget(self.step_view)
 
