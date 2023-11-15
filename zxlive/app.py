7f25048d25860d3b42a9957354d0dd03d5a5f59c
Boldi <boldar99@gmail.com>
2023-11-15 12:08:38 +0000

    fix missing import

--- zxlive/app.py
+++ zxlive/app.py
@@ -19,8 +19,8 @@ from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QCommandLineParser
 from PySide6.QtGui import QIcon
 
-#import sys
-#sys.path.insert(0, '../pyzx')  # So that it can find a local copy of pyzx
+import sys
+# sys.path.insert(0, '../pyzx')  # So that it can find a local copy of pyzx
 
 from .mainwindow import MainWindow
 from .common import get_data, GraphT
