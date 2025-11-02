c207192ccbd265f601268fc3115920fc13e1ff9c
Razin Shaikh <razinshaikh@outlook.com>
2025-11-02 00:03:56 +0000

    fixed style issues in sfx.py and tikz.py

--- zxlive/tikz.py
+++ zxlive/tikz.py
@@ -9,7 +9,8 @@ def proof_to_tikz(proof: ProofModel) -> str:
     settings = QSettings("zxlive", "zxlive")
     vspace = get_settings_value("tikz/layout/vspace", float, settings=settings)
     hspace = get_settings_value("tikz/layout/hspace", float, settings=settings)
-    max_width = get_settings_value("tikz/layout/max-width", float, settings=settings)
+    max_width = get_settings_value(
+        "tikz/layout/max-width", float, settings=settings)
     draw_scalar = False
 
     xoffset = -max_width
@@ -18,8 +19,10 @@ def proof_to_tikz(proof: ProofModel) -> str:
     total_verts, total_edges = [], []
     for i, g in enumerate(proof.graphs()):
         # Compute graph dimensions
-        width = max(g.row(v) for v in g.vertices()) - min(g.row(v) for v in g.vertices())
-        height = max(g.qubit(v) for v in g.vertices()) - min(g.qubit(v) for v in g.vertices())
+        width = (max(g.row(v) for v in g.vertices()) -
+                 min(g.row(v) for v in g.vertices()))
+        height = (max(g.qubit(v) for v in g.vertices()) -
+                  min(g.qubit(v) for v in g.vertices()))
 
         # Translate graph so that the first vertex starts at 0
         min_x = min(g.row(v) for v in g.vertices())
@@ -30,8 +33,12 @@ def proof_to_tikz(proof: ProofModel) -> str:
         if i > 0:
             rewrite = proof.steps[i-1]
             # Try to look up name in settings
-            name = settings.value(f"tikz/names/{rewrite.rule}") if settings.contains(f"tikz/names/{rewrite.rule}") else rewrite.rule
-            eq = f"\\node [style=none] ({idoffset}) at ({xoffset - hspace/2:.2f}, {-yoffset - height/2:.2f}) {{$\\overset{{\\mathit{{{name}}}}}{{=}}$}};"
+            rule_key = f"tikz/names/{rewrite.rule}"
+            name = (settings.value(rule_key) if settings.contains(rule_key)
+                    else rewrite.rule)
+            eq = (f"\\node [style=none] ({idoffset}) at "
+                  f"({xoffset - hspace/2:.2f}, {-yoffset - height/2:.2f}) "
+                  f"{{$\\overset{{\\mathit{{{name}}}}}{{=}}$}};")
             total_verts.append(eq)
             idoffset += 1
 
@@ -48,5 +55,6 @@ def proof_to_tikz(proof: ProofModel) -> str:
         max_index = max(g.vertices()) + 2 * g.num_inputs() + 2
         idoffset += max_index
 
-    return TIKZ_BASE.format(vertices="\n".join(total_verts), edges="\n".join(total_edges))
-
+    return TIKZ_BASE.format(
+        vertices="\n".join(total_verts),
+        edges="\n".join(total_edges))
