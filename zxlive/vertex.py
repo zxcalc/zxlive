f7b5e0be5c6d0db270ad430d98d984535a4e08be
Vihaan agrawal <ninokazlamaz@gmail.com>
2026-06-04 00:45:24 +0530

    Create vertex.py

---
+++ zxlive/vertex.py
@@ -0,0 +1,33 @@
+ADD_VERTEX_GRAPH = {
+    "vertices": {
+        0: (80, 70),
+    },
+    "edges": [],
+}
+
+ADD_EDGE_GRAPH = {
+    "vertices": {
+        0: (60, 70),
+        1: (180, 70),
+    },
+    "edges": [
+        (0, 1),
+    ],
+}
+
+REWRITE_BEFORE = {
+    "vertices": {
+        0: (60, 70),
+        1: (120, 70),
+    },
+    "edges": [
+        (0, 1),
+    ],
+}
+
+REWRITE_AFTER = {
+    "vertices": {
+        0: (90, 70),
+    },
+    "edges": [],
+}
